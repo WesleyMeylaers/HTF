81c1fc26566b0821c0cbd94e60ac336da84336bd
WesleyMeylaers <r0929439@student.thomasmore.be>
2024-11-19 15:04:37 +0100

    Code tot 15u

---
+++ ConsoleApp1/ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Define the custom alphabet
+        Dictionary<char, char> alphabet = new Dictionary<char, char>
+        {
+            {'∆', 'A'}, {'⍟', 'B'}, {'◊', 'C'}, {'Ψ', 'D'}, {'Σ', 'E'},
+            {'ϕ', 'F'}, {'Ω', 'G'}, {'λ', 'H'}, {'ζ', 'I'}, {'Ϭ', 'J'},
+            {'ↄ', 'K'}, {'◯', 'L'}, {'⧖', 'M'}, {'⊗', 'N'}, {'⊕', 'O'},
+            {'∇', 'P'}, {'⟁', 'Q'}, {'⎍', 'R'}, {'φ', 'S'}, {'✦', 'T'},
+            {'⨅', 'U'}, {'ᚦ', 'V'}, {'ϡ', 'W'}, {'⍾', 'X'}, {'⍝', 'Y'},
+            {'≈', 'Z'}
+        };
+
+        // Encoded message
+        string encodedMessage = "ϡλΣ⊗ λ⨅⧖∆⊗ζ✦⍝ ⧖∆ΨΣ ϕζ⎍φ✦ ◊⊕⊗✦∆◊✦ ϡζ✦λ ✦λΣ φ∇Σ◊ζΣφ ↄ⊗⊕ϡ⊗ ⊕⊗◯⍝ ∆φ '✦λΣ ϡ∆✦◊λΣ⎍φ,' ζ✦ ⍟Σ◊∆⧖Σ ◊◯Σ∆⎍ ✦λ∆✦ ✦λΣ⍝ λ∆Ψ ⊕⍟φΣ⎍ᚦΣΨ Σ∆⎍✦λ ϕ⊕⎍ ⧖ζ◯◯Σ⊗⊗ζ∆, ⊗⊕✦ ζ⊗✦Σ⎍ϕΣ⎍ζ⊗Ω, ⍟⨅✦ φ⨅⍟✦◯⍝ Ω⨅ζΨζ⊗Ω Σᚦ⊕◯⨅✦ζ⊕⊗, Σ⊗φ⨅⎍ζ⊗Ω ✦λΣ φ⨅⎍ᚦζᚦ∆◯ ⊕ϕ ✦λΣ φ∇Σ◊ζΣφ ✦λΣ⍝ λ∆Ψ ◊λ⊕φΣ⊗ ✦⊕ ∇⎍⊕✦Σ◊✦.";
+
+        // Decode the message
+        string decodedMessage = DecodeMessage(encodedMessage, alphabet);
+
+        // Print the decoded message
+        Console.WriteLine("Decoded Message: " + decodedMessage);
+    }
+
+    static string DecodeMessage(string message, Dictionary<char, char> alphabet)
+    {
+        string decoded = "";
+        foreach (char c in message)
+        {
+            if (alphabet.ContainsKey(c))
+            {
+                decoded += alphabet[c];
+            }
+            else
+            {
+                decoded += c; // Keep spaces and unknown characters as is
+            }
+        }
+        return decoded;
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/JsonToCode/Program.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+public class Alien
+{
+    public int Strength { get; set; }
+    public int Speed { get; set; }
+    public int Health { get; set; }
+
+    public Alien(int strength, int speed, int health)
+    {
+        Strength = strength;
+        Speed = speed;
+        Health = health;
+    }
+}
+
+public class TeamData
+{
+    public List<Alien> TeamA { get; set; }
+    public List<Alien> TeamB { get; set; }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        string jsonFilePath = "C:/Users/Eigenaar/Desktop/!.NET_Advanced/Oef/ConsoleApp1/JsonToCode/response_1732023162714.json"; // Vervang door het juiste pad naar je JSON-bestand
+
+        // Lees de JSON-data uit het bestand
+        string json = File.ReadAllText(jsonFilePath);
+
+        // Deserialiseer de JSON naar een TeamData-object
+        TeamData teamData = JsonConvert.DeserializeObject<TeamData>(json);
+
+        var teamA = teamData.TeamA;
+        var teamB = teamData.TeamB;
+        Console.WriteLine("Team A");
+        // Doe iets met de data
+        foreach (var alien in teamA)
+        {
+            Console.WriteLine($"new Alien({alien.Strength}, {alien.Speed}, {alien.Health}),");
+        }
+        Console.WriteLine("Team B");
+        foreach (var alien in teamB)
+        {
+            Console.WriteLine($"new Alien({alien.Strength}, {alien.Speed}, {alien.Health}),");
+        }
+    }
+}
